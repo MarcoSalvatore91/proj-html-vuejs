2e48c5e95afb9d54ab4a64354cf04353e9568161
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-18 14:22:55 +0100

    data header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :navBar="navBar" :navPage="navPage" :headImage="headImage"/>
     <Main />
     <Footer />
   </div>
@@ -13,11 +13,40 @@ import Footer from "./components/Footer.vue"
 
 export default {
   name: "App",
+
   components: {
     Header,
     Main,
     Footer,
   },
+
+  data() {
+    return {
+
+      navBar: [{
+        order: "ORDER ONLINE",
+        home: "HOME",
+        pages: "PAGES",
+        menu: "MENU",
+        image: "h5-logo-divided-header.png",
+        event: "EVENT",
+        blog: "BLOG",
+        landing: "LANDING",
+        cart: "CART",
+        search: "SEARCH",
+      }],
+
+      navPage: [{
+        prev: "PREV",
+        next: "NEXT",
+      }],
+
+      headImage: [{
+        bgImage: "h3-rev-img-",
+        image: "h3-rev-img-"
+      }]
+    }
+  }
 };
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,55 @@
 <template>
   <section>
+      <!-- NavBar -->
+      <div class="nav-bar row align-items-center" v-for="nav in navBar" :key="nav.id">
+          <div class="col-2">
+            <a href="#">{{ nav.order }}</a>
+          </div>
 
+          <div class="col-8 d-flex align-items-center justify-content-between">
+            <a href="#">{{ nav.home }}</a>
+            <a href="#">{{ nav.pages }}</a>
+            <a href="#">{{ nav.menu }}</a>
+            <img :src="require (`../assets/img/${nav.image}`)" alt="">
+            <a href="#">{{ nav.event }}</a>
+            <a href="#">{{ nav.blog }}</a>
+            <a href="#">{{ nav.landing }}</a>
+          </div>
+
+          <div class="col-2 d-flex align-items-center justify-content-end">
+            <a href="#">{{ nav.cart }}</a>
+            <a href="#">{{ nav.search }}</a>
+          </div>
+      </div>
+
+      <!-- Sub-NavBar -->
+      <div>
+          
+      </div>
   </section>
 </template>
 
 <script>
 export default {
     name: "Header",
+
+    props: ["navBar"],
 }
 </script>
 
 <style scoped lang="scss">
+@import '../assets/scss/style.scss';
 
+.nav-bar {
+    width: $width-min;
+    margin: $content-center;
+}
+
+img {
+    width: 200px;
+}
+
+a {
+    text-decoration: $decoration-none;
+}
 </style>
