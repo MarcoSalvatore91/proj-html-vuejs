e3721e768ac58e41b075ae40539cedef3a852d82
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-18 17:13:31 +0100

    try to make a image dynamic

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,22 @@
 <template>
   <section>
-      
+    <div class="d-flex row">
+        <div class="col-3 my-2">
+            <img :src="require (`${format}${item}.${type}`)" alt="">
+        </div>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
     name: "Card",
+
+    props: ["format", "item", "type"],
 }
 </script>
 
 <style scoped lang="scss">
+@import '../assets/scss/style.scss';
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,33 @@
 <template>
   <section>
       <section>
-        <div class="d-flex row">
+        <div v-for="(image, index) in item" :key="index">
+            <Card :format="format" :item="item[index]" :type="type"/>
+        </div>
+        <!-- <div class="d-flex row">
             <div class="col-3 my-2" v-for="(image, index) in images" :key="index">
                 <img :src="require (`../assets/img/h3-img-${image}.jpg`)" alt="">
             </div>
-        </div>
+        </div> -->
       </section>
   </section>
 </template>
 
 <script>
+import Card from "./Card.vue"
+
 export default {
     name: "Main",
 
+    components: {
+        Card,
+    },
+
     data() {
         return {
-            images: ["1", "2", "3", "4"]
+            format: "../assets/img/h3-img-",
+            item: ["1", "2", "3", "4"],
+            type: "jpg",
         }
     }
 }
