84a41aca2d512772727c6aaa2c0990ad2b9f9b19
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-18 16:19:11 +0100

    added first section card in main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,30 @@
 <template>
   <section>
-
+      <section>
+        <div class="d-flex row">
+            <div class="col-3 my-2" v-for="(image, index) in images" :key="index">
+                <img :src="require (`../assets/img/h3-img-${image}.jpg`)" alt="">
+            </div>
+        </div>
+      </section>
   </section>
 </template>
 
 <script>
 export default {
     name: "Main",
+
+    data() {
+        return {
+            images: ["1", "2", "3", "4"]
+        }
+    }
 }
 </script>
 
 <style scoped lang="scss">
-
+@import '../assets/scss/style.scss';
+    img {
+        height: 260px;
+    }
 </style>
