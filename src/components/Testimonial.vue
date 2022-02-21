80dae0e197ac8cc0d8fd710d0c30f0728a7f559c
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-21 12:01:55 +0100

    added component testimonial

---
+++ src/components/Testimonial.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+        <div class="d-flex">
+        <div class="mb-2" v-for="image in images" :key="image">
+            <img :src="require (`../assets/img/h1-team-${image}a.jpg`)" alt="">
+        </div>
+        </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: "Testimonial",
+
+    props: ["images"],
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/scss/style.scss';
+
+img {
+    height: 400px;
+    width: 400px;
+}
+</style>
