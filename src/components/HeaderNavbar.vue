2a6ece42d610526175597fd4ef9a074cdf7d1e22
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-02-22 16:54:51 +0100

    added svg

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -9,18 +9,27 @@
             </div>
 
             <div class="col-8 d-flex align-items-center justify-content-between">
-                <a href="#">{{ nav.home }}</a>
+                <div>
+                    <img class="svg pe-2" src="../assets/svg/svg-0.svg" alt="mustache">
+                    <a href="#">{{ nav.home }}</a>
+                </div>
                 <a href="#">{{ nav.pages }}</a>
                 <a href="#">{{ nav.menu }}</a>
-                <img :src="require (`../assets/img/${nav.image}`)" alt="">
+                <img class="logo" :src="require (`../assets/img/${nav.image}`)" alt="">
                 <a href="#">{{ nav.event }}</a>
                 <a href="#">{{ nav.blog }}</a>
                 <a href="#">{{ nav.landing }}</a>
             </div>
 
             <div class="col-2 d-flex align-items-center justify-content-end">
-                <a href="#">{{ nav.cart }}</a>
-                <a href="#" class="ms-4">{{ nav.search }}</a>
+                <div>
+                    <img class="cart svg pe-2" src="../assets/svg/svg-1.svg" alt="cart">
+                    <a class="pe-4" href="#">{{ nav.cart }}</a>
+                </div>
+                <div>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <a href="#" class="ms-4">{{ nav.search }}</a>
+                </div>
             </div>
         </div>
       </section>
@@ -46,6 +55,21 @@ a {
     font-weight: bold;
 }
 
+.svg {
+    height: 10px;
+    filter: invert(100%) sepia(0%) saturate(7494%) hue-rotate(25deg) brightness(97%) contrast(99%);
+
+}
+
+.cart {
+    height: 20px;
+}
+
+i {
+    margin-right: -15px;
+    color: white;
+}
+
 /* Nav-Bar */
 #nav-bar-id {
 
@@ -60,7 +84,7 @@ a {
         width: $width-min;
         margin: $content-center;
         
-        img {
+        .logo {
             width: 200px;
         }
     }
